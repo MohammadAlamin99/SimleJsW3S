4ba35e2ab26dae2ad8de6927a4c96af3dae77ff7
mohammadalamin99 <alamin.cse.99@gmail.com>
2024-01-29 16:02:44 +0600

    add some code

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
-console.log("New windows file ready")
+// ===========> Best practice for js. w3shcools
+
+//  js array iteration.
+// forEach
+    // const numbers = [45, 4, 9, 16, 25];
+    // numbers.forEach(myFunc)
+
+    // function myFunc(value, index, array){
+    //    console.log(value);
+    //    console.log(index);
+    //    console.log(array);
+    //    console.log("-----------------");
+    // }
+
+
+
+
+
+    // arry map
+
+//     const numbers1 = [45, 4, 9, 16, 25];
+//     const numbers2 = numbers1.map(myFunc)
+//     function myFunc(value){
+//         return value*2
+//     }
+
+// console.log(numbers2)
+
+
+
+
+
+// array filter
+
+// const numbers = [45, 4, 9, 16, 25];
+// const over18 = numbers.filter(myFunction);
+
+// function myFunction(value, index, array) {
+//   return value > 18;
+// } 
+// console.log(over18)
+
+
+
+
+// array reduce
+
+// reduce holo soto kora. eita muloto array return kore na...borong eita array theke kisu
+//  ekta kore soto kore like dhoren . eita array er sob sum ber kora hoise
+// const numbers = [45, 4, 9, 16, 25];
+// let sum = numbers.reduce(myFunction);
+
+// function myFunction(total, value, index, array) {
+//     // console.log(total)
+//     // console.log(value)
+//   return total + value;
+// } 
+
+// console.log(sum)
+
+
+
+
+
+// array spreed
+
+// spreed holo bistar kora...
+
+
+// const q1 = ["Jan", "Feb", "Mar"];
+// const q2 = ["Apr", "May", "Jun"];
+// const q3 = ["Jul", "Aug", "Sep"];
+// const q4 = ["Oct", "Nov", "May"];
+
+// const year = [...q1, ...q2, ...q3, ...q4];
+// console.log(year);
+
+
+
+
+// Returns a random integer from 0 to 9
+
+
+
+// let a = Math.floor(Math.random() * 10);
+// console.log(a);
+
+
+
+
+// Create a Map
+// const fruits = new Map()
+
+// fruits.set("apples", 500);
+// fruits.set("bananas", 300);
+// fruits.set("oranges", 200);
+//   console.log(fruits);
+
